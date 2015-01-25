c57a3d167daf10120df926a03eda4e80e336dd5e
corylstewart <corylstewart@gmail.com>
2015-01-25 10:13:08 -0800

    fixed left right problem still not working with repeated integer values

--- top_k.py
+++ top_k.py
@@ -1,31 +1,39 @@
 import random
-
-a = [random.randint(0,10000) for x in xrange(5000)]
+import math
 
 def top_k(a, k):
+    global count
+    count += len(a)
     if len(a) < k:
         return None
     i = random.randint(0,len(a)-1)
     left = []
     right = []
-    middle = []
     center = a[i]
+    right.append(center)
     for j in xrange(len(a)):
-        if a[j] < center:
+        if i == j:
+           pass
+        elif a[j] < center:
             left.append(a[j])
-        elif a[j] == center:
-            middle.append(a[j])
         else:
             right.append(a[j])
-    if len(right) == k-1:
+    if len(right) == k:
         return center
-    elif len(right) >= k:
+    elif len(right) > k:
         return top_k(right,k)
     else:
-        return top_k(left+middle, k-len(right))
+        return top_k(left, k-len(right))
 
-
-k = random.randint(1,5000)
-print top_k(a,k)
-a.sort()
-print a[-k]
+for i in range(100):
+    a = [random.randint(0,10000) for x in xrange(5000)]
+    a = set(a)
+    a = list(a)
+    count = 0
+    k = random.randint(1,len(a))
+    x = top_k(a,k)
+    a.sort()
+    y = a[-k]
+    if x != y:
+        print k, a
+print 'done'
