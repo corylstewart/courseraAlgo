3f5819173d7f81c67e0301882b0e721230f3ff2e
corylstewart <corylstewart@gmail.com>
2015-01-25 11:01:57 -0800

    fixed repeated integers

--- top_k.py
+++ top_k.py
@@ -16,21 +16,17 @@ def top_k(a, k):
            pass
         elif a[j] < center:
             left.append(a[j])
-        elif a[j] == center:
-            k -= 1
         else:
             right.append(a[j])
     if len(right) == k:
         return center
     elif len(right) > k:
-        return top_k(right,k)
+        return top_k(right[1:],k)
     else:
         return top_k(left, k-len(right))
 
 for i in range(100):
     a = [random.randint(0,10000) for x in xrange(5000)]
-    a = set(a)
-    a = list(a)
     count = 0
     k = random.randint(1,len(a))
     x = top_k(a,k)
