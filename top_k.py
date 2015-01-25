26a9b0439738bf5d8ab90e7bf71da7f614ceeed2
corylstewart <corylstewart@gmail.com>
2015-01-25 09:35:34 -0800

    finished top_k

--- top_k.py
+++ top_k.py
@@ -1,11 +1,31 @@
 import random
 
-a = [1,2,3,4,5,100,45,34,67,43,-100]
+a = [random.randint(0,10000) for x in xrange(5000)]
 
 def top_k(a, k):
+    if len(a) < k:
+        return None
     i = random.randint(0,len(a)-1)
     left = []
     right = []
     middle = []
+    center = a[i]
+    for j in xrange(len(a)):
+        if a[j] < center:
+            left.append(a[j])
+        elif a[j] == center:
+            middle.append(a[j])
+        else:
+            right.append(a[j])
+    if len(right) == k-1:
+        return center
+    elif len(right) >= k:
+        return top_k(right,k)
+    else:
+        return top_k(left+middle, k-len(right))
 
-print top_k(a,2)
+
+k = random.randint(1,5000)
+print top_k(a,k)
+a.sort()
+print a[-k]
