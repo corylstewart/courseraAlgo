4271d2442de4f3b8d4baa624880de96f3548c3db
corylstewart <corylstewart@gmail.com>
2015-01-26 14:27:58 -0800

    added quick sort in place

--- sorting.py
+++ sorting.py
@@ -1,6 +1,6 @@
 import random
 
-a = [6,3,7,4,6,8,2,6,43,-99,1]
+a = [3,7,4,66,8,2,6,43,-99,1]
 
 def quick_sort(a):
     if len(a) < 2:
@@ -18,3 +18,22 @@ def quick_sort(a):
         else:
             middle.append(a[i])
     return quick_sort(left) + middle + quick_sort(right)
+
+def quick_sort_in_place(a):
+    if len(a) < 2:
+        return a
+    pivot = random.randint(0,len(a)-1)
+    a[0], a[pivot] = a[pivot], a[0]
+    i = 1
+    j = 1
+    while j < len(a):
+        if a[j] < a[0]:
+            a[i], a[j] = a[j], a[i]
+            i += 1
+        j += 1
+    return quick_sort_in_place(a[1:i]) + [a[0]] + quick_sort_in_place(a[i:]) 
+
+def make_pivot(a):
+    return 
+
+print quick_sort_in_place(a)
