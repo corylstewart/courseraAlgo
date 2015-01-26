ad1bbc2f6a99ea4d2daac7870ab47200e653509b
corylstewart <corylstewart@gmail.com>
2015-01-26 08:39:27 -0800

    added other_way seems to work better

--- array_index_quals_i.py
+++ array_index_quals_i.py
@@ -3,16 +3,18 @@ array A of n distinct integers which can be positive, negative, or zero.
 You want to decide whether or not there is an index i such that A[i] = i. 
 Design the fastest algorithm that you can for solving this problem. '''
 import random
-a = [-5, -3, 0, 1, 2]
+import time
+a = [-5, 6, 7, 8, 9]
 b = [-4, -2, -1, 0]
 
 
 
 def i_equals_index(a, index_start=0):
+    #print a, index_start
     if a[0] == index_start:
         print a[0]
         return True
-    if len(a) > 1:
+    if len(a) > 1 and a[0] < index_start:
         center_index = len(a)/2
         left = a[:center_index]
         right = a[center_index:]
@@ -28,7 +30,7 @@ def i_equals_negative_index(a, index_reverse=-1):
     if a[-1] == index_reverse:
         print a[-1]
         return True
-    if len(a) > 1:
+    if len(a) > 1 and a[-1] > index_reverse:
         center_index = (len(a)/2)
         left = a[:center_index]
         right = a[center_index:]
@@ -55,17 +57,49 @@ def brute_force(a):
             return True
     return False
 
-print does_index_match(a)
-print does_index_match(b)
+def other_way(a):
+    front = True
+    back = True
+    i = 0
+    while i < len(a) and (front or back):
+        if a[i] > i:
+            front = False
+        if a[-i-1] < -i-1:
+            back = False
+        if a[i] == i:
+            print i, 'other'
+            return True
+        if a[-i-1] == -i-1:
+            print -i-1, 'other'
+            return True
+        i += 1
+    return False
 
 
-for i in range(1000):
-    a = [random.randint(-100, 100) for x in range(50)]
+
+#print other_way(a)
+#print does_index_match(b)
+fast = 0
+slow = 0
+maybe = 0
+
+
+for i in range(10000):
+    a = [random.randint(-10000000, 10000000) for x in range(5000)]
     a = set(a)
     a = list(a)
     a.sort()
+
+    start = time.time()
+    x = does_index_match(a)
+    fast += time.time() - start
+    start = time.time()
     y =  brute_force(a)
-    if y:
-        x = does_index_match(a)
-        if x is None:
-            x = False
+    slow += time.time() - start
+    start = time.time()
+    z =  other_way(a)
+    maybe += time.time() - start
+
+print fast
+print slow
+print maybe
